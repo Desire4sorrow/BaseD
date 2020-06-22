83a352a88ecda53be24684362bd9ade5915e593f
Ruslan <56004655+Desire4sorrow@users.noreply.github.com>
2020-06-22 15:44:45 +0300

    update LW3

--- LW3.sql
+++ LW3.sql
@@ -1,69 +1,69 @@
---INSERT(Включение) 
---1.1. Без указания списка полей.
+--INSERT(Р’РєР»СЋС‡РµРЅРёРµ) 
+--1.1. Р‘РµР· СѓРєР°Р·Р°РЅРёСЏ СЃРїРёСЃРєР° РїРѕР»РµР№.
 INSERT INTO stadium VALUES ('Moskow', 100, 10000, 'winter');
---1.2. С указанием списка полей.
+--1.2. РЎ СѓРєР°Р·Р°РЅРёРµРј СЃРїРёСЃРєР° РїРѕР»РµР№.
 INSERT INTO perfomance (price, date, duration)
 VALUES ('1000', 2012-10-20,'00:01:23');
---1.3. С чтением значения из другой таблицы.
+--1.3. РЎ С‡С‚РµРЅРёРµРј Р·РЅР°С‡РµРЅРёСЏ РёР· РґСЂСѓРіРѕР№ С‚Р°Р±Р»РёС†С‹.
 INSERT INTO sportsman_on_competition(id_stadium) SELECT id_stadium FROM stadium;
 
---DELETE(удаление записей) 
---2.1. Всех записей.
+--DELETE(СѓРґР°Р»РµРЅРёРµ Р·Р°РїРёСЃРµР№) 
+--2.1. Р’СЃРµС… Р·Р°РїРёСЃРµР№.
 DELETE stadium;
---2.2. По условию. 
+--2.2. РџРѕ СѓСЃР»РѕРІРёСЋ. 
 DELETE FROM perfomance WHERE price < 1000;
---2.3. Очистить таблицу.
+--2.3. РћС‡РёСЃС‚РёС‚СЊ С‚Р°Р±Р»РёС†Сѓ.
 TRUNCATE TABLE perfomance;
 
---UPDATE(дополнение, изменения в уже существующих записях)
---3.1. Всех записей.
+--UPDATE(РґРѕРїРѕР»РЅРµРЅРёРµ, РёР·РјРµРЅРµРЅРёСЏ РІ СѓР¶Рµ СЃСѓС‰РµСЃС‚РІСѓСЋС‰РёС… Р·Р°РїРёСЃСЏС…)
+--3.1. Р’СЃРµС… Р·Р°РїРёСЃРµР№.
 UPDATE competition SET location = 'Moskow';
---3.2. По условию обновляя атрибут.
+--3.2. РџРѕ СѓСЃР»РѕРІРёСЋ РѕР±РЅРѕРІР»СЏСЏ Р°С‚СЂРёР±СѓС‚.
 UPDATE competition SET name = 'Championship' WHERE location = 'Kazan';
---3.3. По условию обновляя несколько атрибутов.
+--3.3. РџРѕ СѓСЃР»РѕРІРёСЋ РѕР±РЅРѕРІР»СЏСЏ РЅРµСЃРєРѕР»СЊРєРѕ Р°С‚СЂРёР±СѓС‚РѕРІ.
 UPDATE competition SET name = 'Championship', location = 'Kazan' WHERE date = '2020-12-12';
 
 --SELECT 
---4.1. С определенным набором извлекаемых атрибутов.
+--4.1. РЎ РѕРїСЂРµРґРµР»РµРЅРЅС‹Рј РЅР°Р±РѕСЂРѕРј РёР·РІР»РµРєР°РµРјС‹С… Р°С‚СЂРёР±СѓС‚РѕРІ.
 SELECT first_name, last_name, gender FROM sportsman;
---4.2. Со всеми атрибутами в таблице. 
+--4.2. РЎРѕ РІСЃРµРјРё Р°С‚СЂРёР±СѓС‚Р°РјРё РІ С‚Р°Р±Р»РёС†Рµ. 
 SELECT * FROM sportsman;
---4.3. С условием по атрибуту. 
-SELECT * FROM sportsman WHERE first_name = 'Иван';
+--4.3. РЎ СѓСЃР»РѕРІРёРµРј РїРѕ Р°С‚СЂРёР±СѓС‚Сѓ. 
+SELECT * FROM sportsman WHERE first_name = 'РРІР°РЅ';
 
 --SELECT ORDER BY + TOP(LIMIT)
---5.1. С сортировкой по возрастанию ASC + ограничение вывода количества записей.
+--5.1. РЎ СЃРѕСЂС‚РёСЂРѕРІРєРѕР№ РїРѕ РІРѕР·СЂР°СЃС‚Р°РЅРёСЋ ASC + РѕРіСЂР°РЅРёС‡РµРЅРёРµ РІС‹РІРѕРґР° РєРѕР»РёС‡РµСЃС‚РІР° Р·Р°РїРёСЃРµР№.
 SELECT * FROM sportsman_on_competition ORDER BY amount ASC;
---5.2. С сортировкой по убыванию DESC.
+--5.2. РЎ СЃРѕСЂС‚РёСЂРѕРІРєРѕР№ РїРѕ СѓР±С‹РІР°РЅРёСЋ DESC.
 SELECT * FROM sportsman_on_competition ORDER BY position DESC;
---5.3. С сортировкой по двум атрибутам + ограничение вывода количества записей.
+--5.3. РЎ СЃРѕСЂС‚РёСЂРѕРІРєРѕР№ РїРѕ РґРІСѓРј Р°С‚СЂРёР±СѓС‚Р°Рј + РѕРіСЂР°РЅРёС‡РµРЅРёРµ РІС‹РІРѕРґР° РєРѕР»РёС‡РµСЃС‚РІР° Р·Р°РїРёСЃРµР№.
 SELECT TOP 4 * FROM sportsman_on_competition ORDER BY amount DESC, position DESC;
---5.4. С сортировкой по первому атрибуту, из списка извлекаемых, относительная позиция.
+--5.4. РЎ СЃРѕСЂС‚РёСЂРѕРІРєРѕР№ РїРѕ РїРµСЂРІРѕРјСѓ Р°С‚СЂРёР±СѓС‚Сѓ, РёР· СЃРїРёСЃРєР° РёР·РІР»РµРєР°РµРјС‹С…, РѕС‚РЅРѕСЃРёС‚РµР»СЊРЅР°СЏ РїРѕР·РёС†РёСЏ.
 SELECT * FROM sportsman_on_competition ORDER BY 1 DESC;
 
---Работа с датами. Необходимо, чтобы одна из таблиц содержала атрибут с типом DATETIME.
---6.1. WHERE по искомой дате. 
+--Р Р°Р±РѕС‚Р° СЃ РґР°С‚Р°РјРё. РќРµРѕР±С…РѕРґРёРјРѕ, С‡С‚РѕР±С‹ РѕРґРЅР° РёР· С‚Р°Р±Р»РёС† СЃРѕРґРµСЂР¶Р°Р»Р° Р°С‚СЂРёР±СѓС‚ СЃ С‚РёРїРѕРј DATETIME.
+--6.1. WHERE РїРѕ РёСЃРєРѕРјРѕР№ РґР°С‚Рµ. 
 SELECT * FROM sportsman WHERE birthday = '2000-12-12';
---6.2. Извлечь только год из таблицы.
+--6.2. РР·РІР»РµС‡СЊ С‚РѕР»СЊРєРѕ РіРѕРґ РёР· С‚Р°Р±Р»РёС†С‹.
 SELECT YEAR(birthday) FROM sportsman;
 
---SELECT GROUP BY с функциями агрегации.
+--SELECT GROUP BY СЃ С„СѓРЅРєС†РёСЏРјРё Р°РіСЂРµРіР°С†РёРё.
 --7.1. MINIMUM 
 SELECT id_sportsman_on_competition, MIN(amount) AS min_amount FROM sportsman_on_competition GROUP BY id_sportsman_on_competition;
 --7.2. MAXIMUM
 SELECT id_sportsman_on_competition, MAX(position) AS max_position FROM sportsman_on_competition GROUP BY id_sportsman_on_competition;
---7.3. AVG - среднее значение столбца в таблице 
+--7.3. AVG - СЃСЂРµРґРЅРµРµ Р·РЅР°С‡РµРЅРёРµ СЃС‚РѕР»Р±С†Р° РІ С‚Р°Р±Р»РёС†Рµ 
 SELECT id_stadium, AVG(capacity) AS avg_capacity FROM stadium GROUP BY id_stadium;
 --7.4. SUM 
 SELECT id_stadium, SUM(capacity) AS sum_capacity FROM stadium GROUP BY id_stadium;
---7.5. COUNT - количество значений
+--7.5. COUNT - РєРѕР»РёС‡РµСЃС‚РІРѕ Р·РЅР°С‡РµРЅРёР№
 SELECT specialization, COUNT(capacity) AS count_capacity FROM stadium GROUP BY specialization;
 
 --SELECT GROUP BY + HAVING
---8.1. 3 запроса с GROUP BY + HAVING
-SELECT id_stadium, COUNT(capacity) AS count_capacity FROM stadium GROUP BY id_stadium HAVING COUNT(capacity) > 5000; --вывод стадионов, вместимость которых больше 5 тысяч.
-SELECT placement, AVG(capacity) AS avg_capacity FROM stadium GROUP BY placement HAVING AVG(capacity) = 5000; --список городов с средней вместимостью стадионов 5000.
-SELECT placement, COUNT(placement) AS count_same_placement FROM stadium GROUP BY placement HAVING COUNT(placement) > 1; --вывод стадионов с одинаковым расположением.
+--8.1. 3 Р·Р°РїСЂРѕСЃР° СЃ GROUP BY + HAVING
+SELECT id_stadium, COUNT(capacity) AS count_capacity FROM stadium GROUP BY id_stadium HAVING COUNT(capacity) > 5000; --РІС‹РІРѕРґ СЃС‚Р°РґРёРѕРЅРѕРІ, РІРјРµСЃС‚РёРјРѕСЃС‚СЊ РєРѕС‚РѕСЂС‹С… Р±РѕР»СЊС€Рµ 5 С‚С‹СЃСЏС‡.
+SELECT placement, AVG(capacity) AS avg_capacity FROM stadium GROUP BY placement HAVING AVG(capacity) = 5000; --СЃРїРёСЃРѕРє РіРѕСЂРѕРґРѕРІ СЃ СЃСЂРµРґРЅРµР№ РІРјРµСЃС‚РёРјРѕСЃС‚СЊСЋ СЃС‚Р°РґРёРѕРЅРѕРІ 5000.
+SELECT placement, COUNT(placement) AS count_same_placement FROM stadium GROUP BY placement HAVING COUNT(placement) > 1; --РІС‹РІРѕРґ СЃС‚Р°РґРёРѕРЅРѕРІ СЃ РѕРґРёРЅР°РєРѕРІС‹Рј СЂР°СЃРїРѕР»РѕР¶РµРЅРёРµРј.
 
 --SELECT JOIN
 --9.1. LEFT JOIN + WHERE.
@@ -72,15 +72,15 @@ LEFT JOIN sportsman_on_competition ON stadium.id_stadium = sportsman_on_competit
 --9.2. RIGHT JOIN 
 SELECT * FROM stadium 
 RIGHT JOIN sportsman_on_competition ON stadium.id_stadium = sportsman_on_competition.id_stadium;
---9.3. LEFT JOIN 3 таблиц + по атрибуту WHERE с каждой таблицы.
+--9.3. LEFT JOIN 3 С‚Р°Р±Р»РёС† + РїРѕ Р°С‚СЂРёР±СѓС‚Сѓ WHERE СЃ РєР°Р¶РґРѕР№ С‚Р°Р±Р»РёС†С‹.
 SELECT * FROM sportsman_on_competition 
 LEFT JOIN sportsman ON sportsman_on_competition.id_sportsman_on_competition = sportsman.id_sportsman_on_competition
 LEFT JOIN competition ON sportsman.id_sportsman_on_competition = competition.id_sportsman_on_competition
-WHERE position > 10 AND first_name = 'Евгений' AND id_competition > 3;
---9.4. FULL OUTER JOIN 2 таблиц.
+WHERE position > 10 AND first_name = 'Р•РІРіРµРЅРёР№' AND id_competition > 3;
+--9.4. FULL OUTER JOIN 2 С‚Р°Р±Р»РёС†.
 SELECT * FROM perfomance FULL OUTER JOIN stadium on perfomance.id_perfomance = stadium.id_stadium;
---ПОДЗАПРОСЫ
---Запрос с подзапросом WHERE IN.
-SELECT first_name, last_name, gender, birthday FROM sportsman WHERE id_perfomance IN(SELECT id_perfomance FROM perfomance WHERE date = '2019-12-11'); --Данные спортсмена, выступавшего 11 декабря 19 года.
---SELECT atr1, atr2, (подзапрос) FROM ...
-SELECT date, price, (SELECT id_sportsman_on_competition FROM sportsman WHERE sportsman.id_perfomance = perfomance.id_perfomance) AS id_sportsmans_perfomance FROM perfomance;  --вывод даты, цены и id выступления спортсмена. 
+--РџРћР”Р—РђРџР РћРЎР«
+--Р—Р°РїСЂРѕСЃ СЃ РїРѕРґР·Р°РїСЂРѕСЃРѕРј WHERE IN.
+SELECT first_name, last_name, gender, birthday FROM sportsman WHERE id_perfomance IN(SELECT id_perfomance FROM perfomance WHERE date = '2019-12-11'); --Р”Р°РЅРЅС‹Рµ СЃРїРѕСЂС‚СЃРјРµРЅР°, РІС‹СЃС‚СѓРїР°РІС€РµРіРѕ 11 РґРµРєР°Р±СЂСЏ 19 РіРѕРґР°.
+--SELECT atr1, atr2, (РїРѕРґР·Р°РїСЂРѕСЃ) FROM ...
+SELECT date, price, (SELECT id_sportsman_on_competition FROM sportsman WHERE sportsman.id_perfomance = perfomance.id_perfomance) AS id_sportsmans_perfomance FROM perfomance;  --РІС‹РІРѕРґ РґР°С‚С‹, С†РµРЅС‹ Рё id РІС‹СЃС‚СѓРїР»РµРЅРёСЏ СЃРїРѕСЂС‚СЃРјРµРЅР°. 
