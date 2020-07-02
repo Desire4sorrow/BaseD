e7f331246e213ef7d4804e8cabf1aa0b1cb2e5c5
Ruslan <56004655+Desire4sorrow@users.noreply.github.com>
2020-07-02 14:04:23 +0300

    Исправлено в соответствии с замечаниями от 24.06

--- LW3_2.0.sql
+++ LW3_2.0.sql
@@ -1,69 +1,69 @@
---INSERT(Включение)
---1.1. Без указания списка полей.
+--INSERT(Р’РєР»СЋС‡РµРЅРёРµ)
+--1.1. Р‘РµР· СѓРєР°Р·Р°РЅРёСЏ СЃРїРёСЃРєР° РїРѕР»РµР№.
 INSERT INTO competition VALUES ('2019-12-12', 'Moskow', 'Championship', 'open-air');
---1.2. С указанием списка полей.
+--1.2. РЎ СѓРєР°Р·Р°РЅРёРµРј СЃРїРёСЃРєР° РїРѕР»РµР№.
 INSERT INTO perfomance (duration, start_position, variety) VALUES ('00:21:32.1111111', 5, 'public');
---1.3. С чтением значения из другой таблицы.
+--1.3. РЎ С‡С‚РµРЅРёРµРј Р·РЅР°С‡РµРЅРёСЏ РёР· РґСЂСѓРіРѕР№ С‚Р°Р±Р»РёС†С‹.
 INSERT INTO sportsman_on_competition (id_sportsman, id_competition) SELECT id_sportsman, id_competition FROM sportsman, competition;
 
---DELETE(удаление записей) 
---2.1. Всех записей.
+--DELETE(СѓРґР°Р»РµРЅРёРµ Р·Р°РїРёСЃРµР№) 
+--2.1. Р’СЃРµС… Р·Р°РїРёСЃРµР№.
 DELETE stadium;
---2.2. По условию. 
+--2.2. РџРѕ СѓСЃР»РѕРІРёСЋ. 
 DELETE FROM perfomance WHERE variety = 'public';
---2.3. Очистить таблицу.
+--2.3. РћС‡РёСЃС‚РёС‚СЊ С‚Р°Р±Р»РёС†Сѓ.
 TRUNCATE TABLE perfomance;
 
 
---UPDATE(дополнение, изменения в уже существующих записях)
---3.1. Всех записей.
+--UPDATE(РґРѕРїРѕР»РЅРµРЅРёРµ, РёР·РјРµРЅРµРЅРёСЏ РІ СѓР¶Рµ СЃСѓС‰РµСЃС‚РІСѓСЋС‰РёС… Р·Р°РїРёСЃСЏС…)
+--3.1. Р’СЃРµС… Р·Р°РїРёСЃРµР№.
 UPDATE competition SET city = 'Moskow';
---3.2. По условию обновляя атрибут.
+--3.2. РџРѕ СѓСЃР»РѕРІРёСЋ РѕР±РЅРѕРІР»СЏСЏ Р°С‚СЂРёР±СѓС‚.
 UPDATE competition SET name = 'Championship' WHERE city = 'Kazan';
---3.3. По условию обновляя несколько атрибутов.
+--3.3. РџРѕ СѓСЃР»РѕРІРёСЋ РѕР±РЅРѕРІР»СЏСЏ РЅРµСЃРєРѕР»СЊРєРѕ Р°С‚СЂРёР±СѓС‚РѕРІ.
 UPDATE competition SET name = 'Championship', city = 'Kazan' WHERE date = '2019-12-12';
 --SELECT 
---4.1. С определенным набором извлекаемых атрибутов.
+--4.1. РЎ РѕРїСЂРµРґРµР»РµРЅРЅС‹Рј РЅР°Р±РѕСЂРѕРј РёР·РІР»РµРєР°РµРјС‹С… Р°С‚СЂРёР±СѓС‚РѕРІ.
 SELECT first_name, last_name, gender FROM sportsman;
---4.2. Со всеми атрибутами в таблице. 
+--4.2. РЎРѕ РІСЃРµРјРё Р°С‚СЂРёР±СѓС‚Р°РјРё РІ С‚Р°Р±Р»РёС†Рµ. 
 SELECT * FROM sportsman;
---4.3. С условием по атрибуту. 
-SELECT * FROM sportsman WHERE first_name = 'Иван';
+--4.3. РЎ СѓСЃР»РѕРІРёРµРј РїРѕ Р°С‚СЂРёР±СѓС‚Сѓ. 
+SELECT * FROM sportsman WHERE first_name = 'РРІР°РЅ';
 
 
 --SELECT ORDER BY + TOP(LIMIT)
 
---5.1. С сортировкой по возрастанию ASC + ограничение вывода количества записей.
+--5.1. РЎ СЃРѕСЂС‚РёСЂРѕРІРєРѕР№ РїРѕ РІРѕР·СЂР°СЃС‚Р°РЅРёСЋ ASC + РѕРіСЂР°РЅРёС‡РµРЅРёРµ РІС‹РІРѕРґР° РєРѕР»РёС‡РµСЃС‚РІР° Р·Р°РїРёСЃРµР№.
 
 SELECT TOP 3 * FROM sportsman ORDER BY age ASC;
 
---5.2. С сортировкой по убыванию DESC.
+--5.2. РЎ СЃРѕСЂС‚РёСЂРѕРІРєРѕР№ РїРѕ СѓР±С‹РІР°РЅРёСЋ DESC.
 
 SELECT * FROM sportsman ORDER BY age DESC;
 
---5.3. С сортировкой по двум атрибутам + ограничение вывода количества записей.
+--5.3. РЎ СЃРѕСЂС‚РёСЂРѕРІРєРѕР№ РїРѕ РґРІСѓРј Р°С‚СЂРёР±СѓС‚Р°Рј + РѕРіСЂР°РЅРёС‡РµРЅРёРµ РІС‹РІРѕРґР° РєРѕР»РёС‡РµСЃС‚РІР° Р·Р°РїРёСЃРµР№.
 
 SELECT TOP 4 * FROM sportsman ORDER BY last_name, age DESC;
 
---5.4. С сортировкой по первому атрибуту, из списка извлекаемых, относительная позиция.
+--5.4. РЎ СЃРѕСЂС‚РёСЂРѕРІРєРѕР№ РїРѕ РїРµСЂРІРѕРјСѓ Р°С‚СЂРёР±СѓС‚Сѓ, РёР· СЃРїРёСЃРєР° РёР·РІР»РµРєР°РµРјС‹С…, РѕС‚РЅРѕСЃРёС‚РµР»СЊРЅР°СЏ РїРѕР·РёС†РёСЏ.
 
 SELECT * FROM sportsman ORDER BY 1 DESC;
 
 
 
---Работа с датами. Необходимо, чтобы одна из таблиц содержала атрибут с типом DATETIME.
+--Р Р°Р±РѕС‚Р° СЃ РґР°С‚Р°РјРё. РќРµРѕР±С…РѕРґРёРјРѕ, С‡С‚РѕР±С‹ РѕРґРЅР° РёР· С‚Р°Р±Р»РёС† СЃРѕРґРµСЂР¶Р°Р»Р° Р°С‚СЂРёР±СѓС‚ СЃ С‚РёРїРѕРј DATETIME.
 
---6.1. WHERE по искомой дате. 
+--6.1. WHERE РїРѕ РёСЃРєРѕРјРѕР№ РґР°С‚Рµ. 
 
 SELECT * FROM competition WHERE date = '2020-12-12';
 
---6.2. Извлечь только год из таблицы.
+--6.2. РР·РІР»РµС‡СЊ С‚РѕР»СЊРєРѕ РіРѕРґ РёР· С‚Р°Р±Р»РёС†С‹.
 
 SELECT YEAR(date) FROM competition;
 
 
 
---SELECT GROUP BY с функциями агрегации.
+--SELECT GROUP BY СЃ С„СѓРЅРєС†РёСЏРјРё Р°РіСЂРµРіР°С†РёРё.
 
 --7.1. MINIMUM 
 
@@ -73,7 +73,7 @@ SELECT first_name, MIN(age) AS 'min_age' FROM sportsman GROUP BY first_name;
 
 SELECT last_name, MAX(age) AS 'max_age' FROM sportsman GROUP BY last_name;
 
---7.3. AVG - среднее значение столбца в таблице 
+--7.3. AVG - СЃСЂРµРґРЅРµРµ Р·РЅР°С‡РµРЅРёРµ СЃС‚РѕР»Р±С†Р° РІ С‚Р°Р±Р»РёС†Рµ 
 
 SELECT last_name, AVG(age) AS 'avg_age' FROM sportsman GROUP BY last_name;
 
@@ -81,7 +81,7 @@ SELECT last_name, AVG(age) AS 'avg_age' FROM sportsman GROUP BY last_name;
 
 SELECT last_name, SUM(age) AS 'sum_age' FROM sportsman GROUP BY last_name;
 
---7.5. COUNT - количество значений
+--7.5. COUNT - РєРѕР»РёС‡РµСЃС‚РІРѕ Р·РЅР°С‡РµРЅРёР№
 
 SELECT last_name, COUNT(first_name) AS 'count_first_name' FROM sportsman GROUP BY last_name;
 
@@ -89,7 +89,7 @@ SELECT last_name, COUNT(first_name) AS 'count_first_name' FROM sportsman GROUP B
 
 --SELECT GROUP BY + HAVING
 
---8.1. 3 запроса с GROUP BY + HAVING
+--8.1. 3 Р·Р°РїСЂРѕСЃР° СЃ GROUP BY + HAVING
 
 SELECT first_name, SUM(age) AS 'sum_age' FROM sportsman GROUP BY first_name HAVING SUM(age) < 100; 
 SELECT last_name, AVG(age) AS 'avg_age' FROM sportsman GROUP BY last_name HAVING AVG(age) > 30;
@@ -110,25 +110,25 @@ SELECT * FROM perfomance
 
 RIGHT JOIN sportsman ON perfomance.id_sportsman = sportsman.id_sportsman ORDER BY id_perfomance ASC;
 
---9.3. LEFT JOIN 3 таблиц + по атрибуту WHERE с каждой таблицы.
+--9.3. LEFT JOIN 3 С‚Р°Р±Р»РёС† + РїРѕ Р°С‚СЂРёР±СѓС‚Сѓ WHERE СЃ РєР°Р¶РґРѕР№ С‚Р°Р±Р»РёС†С‹.
 
 SELECT first_name, last_name FROM sportsman 
 LEFT JOIN sportsman_on_competition ON sportsman_on_competition.id_sportsman = sportsman.id_sportsman
 LEFT JOIN competition ON sportsman_on_competition.id_competition = competition.id_competition 
 WHERE age < 30 AND id_sportsman_on_competition > 1 AND city = 'Moskow';
 
---9.4. FULL OUTER JOIN 2 таблиц.
+--9.4. FULL OUTER JOIN 2 С‚Р°Р±Р»РёС†.
 
 SELECT * FROM perfomance FULL OUTER JOIN stadium on perfomance.id_perfomance = stadium.id_stadium;
 
 
 
---ПОДЗАПРОСЫ
+--РџРћР”Р—РђРџР РћРЎР«
 
---Запрос с подзапросом WHERE IN.
+--Р—Р°РїСЂРѕСЃ СЃ РїРѕРґР·Р°РїСЂРѕСЃРѕРј WHERE IN.
 
-SELECT start_position,variety FROM perfomance WHERE id_sportsman IN(SELECT id_sportsman FROM sportsman WHERE first_name = 'Иван'); 
+SELECT start_position,variety FROM perfomance WHERE id_sportsman IN(SELECT id_sportsman FROM sportsman WHERE first_name = 'РРІР°РЅ'); 
 
---SELECT atr1, atr2, (подзапрос) FROM ...
+--SELECT atr1, atr2, (РїРѕРґР·Р°РїСЂРѕСЃ) FROM ...
 
 SELECT start_position, variety, (SELECT first_name FROM sportsman WHERE perfomance.id_sportsman = sportsman.id_sportsman) AS 'first_name' FROM perfomance;
